7254dc108522e144b2461f80c3a28d236642e621
Minimage <seancampbell914@gmail.com>
2022-04-28 13:18:44 +0000

    Function complete, want to talk to Nav if this is acceptable

--- src/js/component/traffic.jsx
+++ src/js/component/traffic.jsx
@@ -1,13 +1,42 @@
 import React, { useState, useEffect } from "react";
 
 const Lights = () => {
+	const [red, setRed] = useState("red");
+	const [yellow, setYellow] = useState("yellow");
+	const [green, setGreen] = useState("green");
+
+	const clickRed = () => {
+		setRed("blue");
+		setYellow("yellow");
+		setGreen("green");
+	};
+	const clickYellow = () => {
+		setYellow("blue");
+		setRed("red");
+		setGreen("green");
+	};
+	const clickGreen = () => {
+		setGreen("blue");
+		setRed("red");
+		setYellow("yellow");
+	};
+
 	return (
 		<div>
 			<div className="trafficTop mx-auto"></div>
 			<div className="container ">
-				<div className="light red"></div>
-				<div className="light yellow"></div>
-				<div className="light green"></div>
+				<div
+					className="light red"
+					onClick={clickRed}
+					style={{ backgroundColor: red }}></div>
+				<div
+					className="light yellow"
+					onClick={clickYellow}
+					style={{ backgroundColor: yellow }}></div>
+				<div
+					className="light green"
+					onClick={clickGreen}
+					style={{ backgroundColor: green }}></div>
 			</div>
 		</div>
 	);
